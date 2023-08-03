b9afc2638bd5eb66d5ab0291bb97a3a27c1b94f4
DIO <princessnoachi@gmail.com>
2023-08-03 23:21:27 +0800

    修改產品 v2.0

--- public/php/sign/change.php
+++ public/php/sign/change.php
@@ -1,5 +1,20 @@
 <?php
+require('../db2.php');
 
-$cName=$_GET['cName'];
+$cName = $_GET['cName'];
 
-echo $cName;
+$sql = 'select cName,price,kind,cSize,feature,level,material from cake where cName =?';
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param('s', $cName);
+$stmt->execute();
+$result = $stmt->get_result();
+// $row = $result->fetch_assoc();
+// $row=
+
+while ($row = $result->fetch_assoc()) {
+    $res_json = array();
+    $res_json[] = $row;
+    $res_json = json_encode($res_json);
+}
+header("Content-Type: application/json");
+echo $res_json;
