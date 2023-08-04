0bc9c270956b2fd4223e87b11aafa24b28f500ae
oracle9487 <cactus50606@gmail.com>
2023-08-04 15:28:08 +0800

    前端輪播圖及隨機顯示心得完成,後端PHP檔案一併上傳

--- public/php/mainpage/upload.php
+++ public/php/mainpage/upload.php
@@ -1,68 +1,31 @@
 <?php
-require_once('../DB.php');
+require('../db2.php');
 
-?>
-<?php
-// 指定存儲上傳檔案的目錄為 binfo
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["fileToUpload"])) {
-    $targetDir = "binfo/";
+$bid = $_REQUEST['bid'];
+// $text = $_REQUEST['text'];
+$src = $_FILES['file1']['tmp_name'];
+$src1 = $_FILES['file2']['tmp_name'];
+$src2 = $_FILES['file3']['tmp_name'];
+$src3 = $_FILES['file4']['tmp_name'];
+$src4 = $_FILES['file5']['tmp_name'];
+$contents = file_get_contents($src);
+$contents1 = file_get_contents($src1);
+$contents2 = file_get_contents($src2);
+$contents3 = file_get_contents($src3);
+$contents4 = file_get_contents($src4);
 
-    // 取得表單欄位值
-    $bid = $_POST["bid"];
-    $description = $_POST["description"];
+$sql = "update binfo set Img1 = ?,Img2 = ?,Img3 = ?,Img4 = ?,bodyimg = ? where bid = ?";
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param('bbbbbs', $contents, $contents1, $contents2, $contents3, $contents4, $bid);
+$stmt->send_long_data(0, $contents); // send_long_data(a, 變數) a取至於上方的相對位置 (bs) [01]
+$stmt->send_long_data(1, $contents1); // send_long_data(a, 變數) a取至於上方的相對位置 (bs) [01]
+$stmt->send_long_data(2, $contents2); // send_long_data(a, 變數) a取至於上方的相對位置 (bs) [01]
+$stmt->send_long_data(3, $contents3); // send_long_data(a, 變數) a取至於上方的相對位置 (bs) [01]
+$stmt->send_long_data(4, $contents4); // send_long_data(a, 變數) a取至於上方的相對位置 (bs) [01]
 
-    // 上傳 5 張圖片
-    $uploadedFiles = array();
-    $uploadOk = 1;
+$stmt->execute();
 
-    foreach ($_FILES["fileToUpload"]["tmp_name"] as $key => $tmp_name) {
-        $imageFileType = strtolower(pathinfo($_FILES["fileToUpload"]["name"][$key], PATHINFO_EXTENSION));
-        $uniqueFileName = uniqid() . '.' . $imageFileType; // 生成唯一的檔案名稱
+echo "真的修改成功!";
+sleep(5);
 
-        $targetFile = $targetDir . $uniqueFileName;
-
-        // 檢查檔案是否為圖片檔案
-        $check = getimagesize($tmp_name);
-        if ($check === false) {
-            echo "檔案不是一個圖片。";
-            $uploadOk = 0;
-        }
-
-        // 允許指定的檔案格式
-        $allowedFormats = array("jpg", "jpeg", "png", "gif");
-        if (!in_array($imageFileType, $allowedFormats)) {
-            echo "抱歉，只允許上傳 JPG, JPEG, PNG 和 GIF 檔案。";
-            $uploadOk = 0;
-        }
-
-        // 檢查檔案大小限制，設定為 5 MB
-        if ($_FILES["fileToUpload"]["size"][$key] > 5000000) {
-            echo "抱歉，檔案太大。";
-            $uploadOk = 0;
-        }
-
-        if ($uploadOk == 0) {
-            echo "抱歉，您的檔案未被上傳。";
-        } else {
-            if (move_uploaded_file($tmp_name, $targetFile)) {
-                $uploadedFiles[] = $targetFile;
-            } else {
-                echo "抱歉，上傳檔案時發生錯誤。";
-            }
-        }
-    }
-
-    // 如果所有圖片都成功上傳，可以將其資訊存入資料庫
-    if ($uploadOk == 1 && count($uploadedFiles) === 5) {
-
-        // 將圖片路徑組合為一個字串，以逗號分隔
-        $imagePaths = implode(",", $uploadedFiles);
-
-        // 存入資料庫
-        $query = "INSERT INTO your_table_name (bid, body, Img1, Img2, Img3, Img4, bodyImg) 
-                  VALUES ('$bid', '$description', '$imagePaths', '$imagePaths', '$imagePaths', '$imagePaths', '$imagePaths')";
-
-        echo "所有檔案已成功上傳並存入資料庫。";
-    }
-}
-?>
+unlink($src);
