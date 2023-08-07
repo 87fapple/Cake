f7dc912c8dd14e73f2ee825449af066a84b41740
DIO <princessnoachi@gmail.com>
2023-08-08 00:10:49 +0800

    新增圖片預覽V2.0

--- public/php/sign/add.php
+++ public/php/sign/add.php
@@ -15,18 +15,28 @@ $cSize = $_REQUEST['cSize'];
 $price = $_REQUEST['price'];
 $feature = $_REQUEST['feature'];
 $material = $_REQUEST['material'];
-
-$c1 = $_FILES['cImg1']['tmp_name'];
+$cImg1 = $_FILES['cImg1']['tmp_name'];
+$cImg2 = $_FILES['cImg2']['tmp_name'];
 // $c1=new Image($cImg1);
 // $c1->resize(400,400,'fill');
 // list($c1) = $photo->getImageSrc('thumb');
-$cImg2 = $_FILES['cImg2']['tmp_name'];
 
+// $c1 = file_get_contents($c1);
+// $c1 = base64_encode($c1);
+// $c2 = file_get_contents($c2);
 
+if ($_FILES["cImg1"]["error"] > 0 or $_FILES["cImg2"]["error"] > 0) {
+    echo "Return Code: " . $_FILES["cImg1"]["error"] . "<br />";
+    echo "Return Code: " . $_FILES["cImg2"]["error"] . "<br />";
+} else {
+    move_uploaded_file($cImg1,"../../../image/cake_add/".$_FILES["cImg1"]["name"]);
+    $c1 = "../../../image/cake_add/".$_FILES["cImg1"]["name"];
+    echo $c1 ."<br/>";
+    move_uploaded_file($cImg2,"../../../image/cake_add/".$_FILES["cImg2"]["name"]);
+    $c2 = "../../../image/cake_add/".$_FILES["cImg2"]["name"];
+    echo $c2;
+}
 
-$c1 = file_get_contents($c1);
-$c1 = base64_encode($c1);
-$c2 = file_get_contents($cImg2);
 
 $sql="insert into cake(cName,price,kind,cSize,cImg1,cImg2,feature,level,material)
 value(?,?,?,?,?,?,?,?,?)";
@@ -41,9 +51,6 @@ $stmt->execute();
 // value(?,?,?,?,?,?,?,?,?)',[$cName,$price,$kind,$cSize,$c1,$c2,$feature,$level,$material]);
 
 
-
-header('location:/Cake/public/php/sign/add2.html');
-
 //查看圖片內容
 // if (($_FILES["cImg1"]["type"] == "image/gif")
 // || ($_FILES["cImg1"]["type"] == "image/jpeg")
@@ -52,9 +59,19 @@ header('location:/Cake/public/php/sign/add2.html');
 // if ($_FILES["cImg1"]["error"] > 0) {
 //     echo "Return Code: " . $_FILES["cImg1"]["error"] . "<br />";
 // } else {
-//     echo "檔名: " . $_FILES["cImg1"]["name"] . "<br />";
-//     echo "檔案型別: " . $_FILES["cImg1"]["type"] . "<br />";
-//     echo "檔案大小: " . ($_FILES["cImg1"]["size"] / 1024) . " Kb<br />";
-//     echo "快取檔案: " . $_FILES["cImg1"]["tmp_name"] . "<br />";
+    // echo "檔名: " . $_FILES["cImg1"]["name"] . "<br />";
+    // echo "檔案型別: " . $_FILES["cImg1"]["type"] . "<br />";
+    // echo "檔案大小: " . ($_FILES["cImg1"]["size"] / 1024) . " Kb<br />";
+    // echo "快取檔案: " . $_FILES["cImg1"]["tmp_name"] . "<br />";
+
+    // if(file_exists("image/".$_FILES["cImg1"]["name"])){
+    //     echo "檔案已經存在，請勿重複上傳";
+    // }else{
+    // move_uploaded_file($_FILES["cImg1"]["tmp_name"],"../image/".$_FILES["cImg1"]["name"]);
+    // };
+//     echo "image/".$_FILES["cImg1"]["name"];
 // }
 
+// header('location:/Cake/public/php/sign/add2.html');
+header("refresh:3;url=add2.html"); 
+// alert('正在加载，请稍等...3秒後自動跳轉');
--- public/php/sign/change.php
+++ public/php/sign/change.php
@@ -3,38 +3,36 @@ require('../db2.php');
 // require('../DB.php');
 
 
-// $cName = $_GET['cName'];
+$cName = $_GET['cName'];
 
 $sql = "select cName,price,kind,cSize,feature,level,material, cImg1,cImg2 from cake where cName = '巧克力蛋糕'";
 $stmt = $mysqli->prepare($sql);
 // $stmt->bind_param('s', $cName);
 $stmt->execute();
 $result = $stmt->get_result();
-$row =$result->fetch_assoc();
-$image = $row['cImg1'];
-if ($image == null) {
-    $image = file_get_contents('https://cdn0.techbang.com/system/excerpt_images/8428/original/de636579295c2d745894c8daf8af51ae.jpg?1329274269');
+
+$res_json =[];
+while ($row =$result->fetch_assoc()) {
+    $res_json[] = $row;
+    $res_json = json_encode($res_json);
 }
+header("Content-Type: application/json");
+echo $res_json;
+
+// $row =$result->fetch_assoc();
+// $image = $row['cImg1'];
+// if ($image == null) {
+//     $image = file_get_contents('https://cdn0.techbang.com/system/excerpt_images/8428/original/de636579295c2d745894c8daf8af51ae.jpg?1329274269');
+// }
 // data:image/jpeg;base64,ooxx
-$mine_type = (new finfo(FILEINFO_MIME_TYPE))->buffer($image);
-$image_base64 = base64_encode($image);
-$src = "data:{$mine_type};base64,{$image_base64}";
+// $mine_type = (new finfo(FILEINFO_MIME_TYPE))->buffer($image);
+// $image_base64 = base64_encode($image);
+// $src = "data:image/jpeg;base64,{$image_base64}";
 // echo $src;
 // $mine_type = (new finfo(FILEINFO_MIME_TYPE))->buffer($cImg1);
 // $image_base64 = base64_encode($cImg1);
 // $src1 = "data:{$mine_type};base64,{$image_base64}";
 // $src1 = "data:image/jpg;base64,{$image_base64}";
-// header("Content-Type: image/png");
-echo '<img src=' . $src . ' style="max-width: 200px" >';
+// header("Content-Type: image/jpeg");
+// echo '<img src=' . $src . ' style="max-width: 200px" >';
 
-// $res_json =[];
-// while ($row =$result->fetch_assoc()) {
-//     $res_json[] = $row;
-//     // $res_json = array();
-//     $res_json = json_encode($res_json);
-// }
-// header("Content-Type: application/json");
-// echo '<pre/>';
-// var_dump($src1);
-// echo $src1;
-// echo $res_json;?>
