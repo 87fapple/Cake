57014210ac1935ce69b765ceb21d807f27f7aecc
JOJO2 <princessnoachi@gmail.com>
2023-08-09 16:46:22 +0800

    新增忘記密碼v1.0

--- public/php/admin/change_search.php
+++ public/php/admin/change_search.php
@@ -18,21 +18,3 @@ while ($row =$result->fetch_assoc()) {
 $res_json = json_encode($res_json);
 header("Content-Type: application/json");
 echo $res_json;
-
-// $row =$result->fetch_assoc();
-// $image = $row['cImg1'];
-// if ($image == null) {
-//     $image = file_get_contents('https://cdn0.techbang.com/system/excerpt_images/8428/original/de636579295c2d745894c8daf8af51ae.jpg?1329274269');
-// }
-// data:image/jpeg;base64,ooxx
-// $mine_type = (new finfo(FILEINFO_MIME_TYPE))->buffer($image);
-// $image_base64 = base64_encode($image);
-// $src = "data:image/jpeg;base64,{$image_base64}";
-// echo $src;
-// $mine_type = (new finfo(FILEINFO_MIME_TYPE))->buffer($cImg1);
-// $image_base64 = base64_encode($cImg1);
-// $src1 = "data:{$mine_type};base64,{$image_base64}";
-// $src1 = "data:image/jpg;base64,{$image_base64}";
-// header("Content-Type: image/jpeg");
-// echo '<img src=' . $src . ' style="max-width: 200px" >';
-
