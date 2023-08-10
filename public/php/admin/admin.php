6ee90335d3356a197beedb82f75bf9e0d256b1f7
JOJO2 <princessnoachi@gmail.com>
2023-08-10 11:22:12 +0800

    新增商品總攬

--- public/php/admin/admin.php
+++ public/php/admin/admin.php
@@ -1,25 +1,14 @@
 <?php session_start(); ?>
 <?php
-if (!$_COOKIE['token']) {
-    header('Location: /Cake/public/login.html');
-    die();
-}
+// if (!$_COOKIE['token']) {
+//     header('Location: /Cake/public/login.html');
+//     die();
+// }
 
-$uName = $_COOKIE['user'];
 
 require('../db2.php');
 
-$sql = "select userinfo.uName,orders.oid,orders.sid,orders.people,orders.reserveDate,orders.reserveTime,store.location , GROUP_CONCAT(cake.cName,'*',orderlist.num) as cName
-from orders 
-inner join userinfo  on userinfo.uid = orders.uid
-inner join orderlist on orders.oid = orderlist.oid
-inner join cake on orderlist.cid = cake.cid
-inner join store on orders.sid= store.sid
-
- GROUP BY orders.oid
- ORDER BY orders.reserveDate
- ;
-";
+$sql = "select * from cake";
 
 $stmt = $mysqli->prepare($sql);
 $stmt->execute();
@@ -55,29 +44,7 @@ $result = $stmt->get_result();
     <!-- Back-to-Top Button -->
     <button onclick="topFunction()" class="topBtn" id="topBtn"></button>
 
-    <!-- Navbar -->
-    <nav class="navbar">
-        <div class="navbarTitle">
-            <a href="../public/mainpage.html">
-                <img src="../image/icon-noBorder-whiteFont.png">
 
-            </a>
-        </div>
-        <div class="hambuger">
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
-        </div>
-        <div class="navbarLink">
-            <ul>
-                <li><a href="../public/menu.php">產品介紹</a></li>
-                <li><a href="../public/locations.html">分店資訊</a></li>
-                <li><a href="../public/reserve.php">預約課程</a></li>
-                <li><a href="../public/Q&A.html">常見問題</a></li>
-                <li><a href="../public/login.html">登入會員</a></li>
-            </ul>
-        </div>
-    </nav>
 
     <div name="selecttop" id="sel">
         <a href="../public/admin.html" class="selectarea"><i style='font-size:24px' class='fas'>&#xf1b0;</i>&nbsp修改首頁輪播</a>
@@ -93,89 +60,36 @@ $result = $stmt->get_result();
             <h2>預約總覽</h2>
         </div>
 
-        <div class="row">
-            <div class="column">
-                <label for="change_cName" class="selectcake">選擇分店：</label>
-                <select name="change_cName" id="change_cName" class="selectcake">
-                    <option value="">皮卡丘</option>
-                    <option value="">小火龍</option>
-                    <option value="">廟挖馬</option>
-                </select>
-            </div>
-
-            <div class="column">
-                <label for="selectDate" class="selectDate">選擇日期：</label>
-                <input type="text" id="datepicker">
-                </select>
-            </div>
-        </div>
-
         <br>
         <br>
         <table>
             <tr class="mainTable">
-                <th>日期</th>
-                <th>分店</th>
-                <th>時段</th>
-                <th id="products">產品</th>
-                <th>總人數</th>
-                <th>會員名稱</th>
-                <th>訂單狀況</th>
+                <th>蛋糕編號</th>
+                <th>名稱</th>
+                <th>價格</th>
+                <th >種類</th>
+                <th>尺寸</th>
+                <th>難度</th>
+                <th>修改</th>
             </tr>
 
             <?php
             while ($row = $result->fetch_assoc()) {
-                // echo "<pre/>";
-                // var_dump($row);
                 echo
                 '<tr class="mainTable">
-                        <td>' . $row['reserveDate'] . '</td>
-                        <td>' . $row['location'] . '</td>
-                        <td>' . $row['reserveTime'] . '</td>
-                        <td id="products">' . $row['cName'] . '</td>
-                        <td>' . $row['people'] . '人</td>
-                        <td>' . $row['uName'] . '</td>
-                        <td id="button"><button>取消預約</button></td>
+                        <td>' . $row['cid'] . '</td>
+                        <td>' . $row['cName'] . '</td>
+                        <td>' . $row['price'] . '</td>
+                        <td >' . $row['kind'] . '</td>
+                        <td>' . $row['cSize'] . '</td>
+                        <td>' . $row['level'] . '</td>
+                        <td id="button"><button>修改訂單</button></td>
                     </tr>';
             }
             ?>
             <table>
-                <br>
-                <br>
-                <br>
-                <!-- Footer -->
-                <footer class="footer">
-                    <div class="footerContainer">
-                        <div class="footerRow">
-                            <div class="footerCol">
-                                <h4>DIY蛋糕</h4>
-                                <ul>
-                                    <li><a href="">關於我們</a></li>
-                                    <li><a href="">常見問題</a></li>
-                                </ul>
-                            </div>
-                            <div class="footerCol">
-                                <h4>服務內容</h4>
-                                <ul>
-                                    <li><a href="">立即預約</a></li>
-                                    <li><a href="">產品介紹</a></li>
-                                </ul>
-                            </div>
-                            <div class="footerCol">
-                                <h4>聯絡我們</h4>
-                                <div class="socialLinks">
-                                    <a href=""><i class="fab fa-facebook-f"></i></a>
-                                    <a href=""><i class="fab fa-twitter"></i></a>
-                                    <a href=""><i class="fab fa-instagram"></i></a>
-                                    <a href=""><i class="fab fa-line"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
                 <script src="../../../resources/js/topBtn.js"></script>
                 <script src="../../../resources/js/navbar.js"></script>
 
 </body>
-
 </html>
