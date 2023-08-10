02ccc9c3a51de13fddd1bcf563f619f21c15b2a4
JOJO2 <princessnoachi@gmail.com>
2023-08-10 16:23:33 +0800

    修改密碼完成

--- public/php/forget/repwd.php
+++ public/php/forget/repwd.php
@@ -1,6 +1,21 @@
 <?php
-require('php/db2.php');
-
-$pwd=$_REQUEST['pwd'];
-
-DB::update('update userinfo set pwd = ? where token=?');
+header('Refresh:3,url=/Cake/public/login.html');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="//unpkg.com/layui@2.7.6/dist/layui.js"></script>
+</head>
+<body>
+    <script>
+        layer.open({
+        content: '修改完成,3秒後跳轉...',
+        title:(''),
+        scrollbar: false
+});
+    </script>
+</body>
+</html>
