1518ba89211d4108146af256288fd157cfeabf3c
Wei <ikft12031@gmail.com>
2023-08-17 11:05:27 +0800

    後台navbar新增

--- public/CMS/navbar.php
+++ public/CMS/navbar.php
@@ -14,42 +14,42 @@
     <hr>
     <ul>
         <?php
-            $urls = array(
-                #'首頁' => '/Cake/public/CMS/home.php',
-                '首頁管理' => '/Cake/public/CMS/mgt_home.php',
-                '產品管理' => '/Cake/public/CMS/mgt_product.php',
-                '預約管理' => '/Cake/public/CMS/mgt_reserve.php',
-                '會員管理' => '/Cake/public/CMS/mgt_member.php'
-                // …
-            );
-
-            foreach ($urls as $name => $url) {
-                print 
-                    '<li>
-                        <a href="' . $url . '">' . $name . '</a>
-                    </li>';
-            }
-
-            // $names = array('首頁管理', '產品管理', '預約管理', '會員管理');
             // $urls = array(
-            //     '/Cake/public/CMS/mgt_home.php',
-            //     '/Cake/public/CMS/mgt_product.php',
-            //     '/Cake/public/CMS/mgt_reserve.php',
-            //     '/Cake/public/CMS/mgt_member.php');
-            // $icons = array('home', 'cake', 'receipt_long', 'group'); 
-            // $combined = array_map(null, $names, $urls, $icons);
-            // // var_dump($combined);
-            // foreach ($combined as $value) {
-            //     $name = $value[0];
-            //     $url = $value[1];
-            //     $icon = $value[2];
-                
-            //     echo 
-            //         "<li>
-            //             <span class='material-symbols-outlined'>".$icon."</span>
-            //             <a herf='".$url."' />".$name.
-            //         "</li>";
+            //     #'首頁' => '/Cake/public/CMS/home.php',
+            //     '首頁管理' => '/Cake/public/CMS/mgt_home.php',
+            //     '產品管理' => '/Cake/public/CMS/mgt_product.php',
+            //     '預約管理' => '/Cake/public/CMS/mgt_reserve.php',
+            //     '會員管理' => '/Cake/public/CMS/mgt_member.php'
+            //     // …
+            // );
+
+            // foreach ($urls as $name => $url) {
+            //     print 
+            //         '<li>
+            //             <a href="' . $url . '">' . $name . '</a>
+            //         </li>';
             // }
+
+            $names = array('首頁管理', '產品管理', '預約管理', '會員管理');
+            $urls = array(
+                '/Cake/public/CMS/mgt_home.php',
+                '/Cake/public/CMS/mgt_product.php',
+                '/Cake/public/CMS/mgt_reserve.php',
+                '/Cake/public/CMS/mgt_member.php');
+            $icons = array('home', 'cake', 'receipt_long', 'group'); 
+            $combined = array_map(null, $names, $urls, $icons);
+            // var_dump($combined);
+            foreach ($combined as $value) {
+                $name = $value[0];
+                $url = $value[1];
+                $icon = $value[2];
+                
+                echo 
+                    "<li>
+                        <span class='material-symbols-outlined'>".$icon."</span>
+                        <a href='".$url."' class='nav_a'>".$name."</a>
+                    </li>";
+            }
         ?>
     </ul> 
 </nav>
